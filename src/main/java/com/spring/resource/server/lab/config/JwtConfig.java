be9e80b49193bdf85ce2238830879705057866e6
brittanypallasco2003 <nohemiespinel0@gmail.com>
2026-07-28 00:02:57 -0500

    Implementa el convertidor de autenticación JWT y configura la seguridad del recurso

---
+++ src/main/java/com/spring/resource/server/lab/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.spring.resource.server.lab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter() {
+        return new JwtGrantedAuthoritiesConverter();
+    }
+
+}
